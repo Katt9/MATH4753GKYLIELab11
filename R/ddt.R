8ac7ec9af9f1dea738344331763b82dfeaf6f28b
Katt9 <kyliehumble@yahoo.com>
2024-07-24 08:33:34 -0500

    Initial commit of MATH4753GKYLIELab11 package

---
+++ R/ddt.R
@@ -0,0 +1,17 @@
+#' DDT data set
+#'
+#' A complete data set of measurements of 144 fish caught in various rivers
+#'
+#' @format
+#' A data frame with 144 rows and 6 columns:
+#' \describe{
+#'   \item{RIVER}{Capture location}
+#'   \item{MILE}{Distance from the mouth of the river or creek}
+#'   \item{SPECIES}{Species of fish}
+#'   \item{LENGTH}{Length of fish}
+#'   \item{WEIGHT}{Weight of fish}
+#'   \item{DDT}{DDT levels}
+#'   ...
+#' }
+#' @source <https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850>
+"ddt"
